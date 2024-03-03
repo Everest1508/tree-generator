78c15d6e5f44e6d02801f82278bb16b3cdcb6e52
Everest1508 <riteshmahale15@gmail.com>
2024-03-04 01:00:00 +0530

    v1 final commit

--- playground/views.py
+++ playground/views.py
@@ -1,18 +1,63 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 import json
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+
+def parse_input(input_str):
+    lines = input_str.split('\n')
+    root = None
+    nodes = {}
+
+    for line in lines:
+        parts = line.strip().split(' = ')
+        parent_name = parts[0].split('.')[0]
+        child_name = parts[1]
+
+        if parent_name == 'root':
+            root = Node(child_name)
+            nodes[child_name] = root
+        else:
+            parent_node = nodes[parent_name]
+            child_node = Node(child_name)
+            parent_node.children.append(child_node)
+            nodes[child_name] = child_node
+
+    return root
+
+def tree_to_json(node):
+    json_tree = {
+        "name": node.name,
+        "children": [tree_to_json(child) for child in node.children]
+    }
+    return json_tree
+
+
 
 def dynamic_tree_view(request):
     
     dynamic_data = []
+    if request.method == "GET":
+        input_data = ""
     if request.method == 'POST':
+        input_data = ""
         input_data = request.POST.get('dictionaryInput')
-        print(input_data)
-        if input_data:
+        # print(input_data)
+        input_data = input_data = input_data.strip()
+        root = parse_input(input_data)
+        print(root)
+
+        json_tree = tree_to_json(root)
+        print(json_tree)
+        compiled_data ="["+json.dumps(json_tree)+"]"
+        # mymain = []
+        # mymain.append(json_tree)
+        print(compiled_data)
+        if compiled_data:
             try:
-                dynamic_data = json.loads(input_data)
+                dynamic_data = json.loads(compiled_data)
             except json.JSONDecodeError:
                 # Handle invalid JSON data
                 pass
@@ -53,4 +98,4 @@ def dynamic_tree_view(request):
     #         ]
     #     }
     # ]
-    return render(request, 'index.html', {'dynamic_data': dynamic_data})
+    return render(request, 'index.html', {'dynamic_data': dynamic_data,'old_txt':input_data})
